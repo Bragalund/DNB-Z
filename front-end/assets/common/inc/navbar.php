1b0d07d60e433e0c7cb4b98b9d069b9bf2809787
Mathias Tollerud <mathias.tollerud@gmail.com>
2016-10-26 09:14:44 +0200

    Major front-end update. Lots of code!!!

---
+++ front-end/assets/common/inc/navbar.php
@@ -0,0 +1,51 @@
+<?php
+$navLeft = array(
+	'Meny 1'                   => $conf['pages']['menu1'],
+	'Meny 2'                   => $conf['pages']['menu2']
+);
+
+$navRight = array(
+	'Meny 3'                   => $conf['pages']['menu3'],
+	'Meny 4'                   => $conf['pages']['menu4']
+);
+?>
+
+<nav class="navbar navbar-default navbar-custom" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+                <span class="sr-only">Meny</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-left">
+                <?php
+    				foreach($navLeft as $nav_title => $nav_link){
+    					echo '<li class="'.($nav_link == basename($_SERVER['PHP_SELF']) ? 'active':'').'"><a href="'.$conf['pathToRoot'].''.$nav_link.'">'.$nav_title.'</a></li>';
+    				}
+				?>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                    <a href="#hello" class="dropdown">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                    </ul>
+                </li>
+                <?php
+    				foreach($navRight as $nav_title => $nav_link){
+    					echo '<li class="'.($nav_link == basename($_SERVER['PHP_SELF']) ? 'active':'').'"><a href="'.$conf['pathToRoot'].''.$nav_link.'">'.$nav_title.'</a></li>';
+    				}
+				?>
+            </ul>
+        </div><!--/.navbar-collapse -->
+    </div>
+</nav>
--- front-end/index.php
+++ front-end/index.php
@@ -1,3 +1,7 @@
+<?php
+    require_once("global/config.php");
+    $visitorIP = $customClass->getUserIP();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,78 +9,169 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content="Gruppe 15">
 
-    <title>Standard template</title>
+    <title>DNB Z</title>
 
     <!-- Import stylesheets -->
     <link href="assets/common/css/stylesheet.css" rel="stylesheet">
 </head>
-
 <body>
-    <nav class="navbar navbar-inverse navbar-fixed-top">
+    <header>
         <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+            <div class="row valign">
+                <div class="col-md-2 col-sm-3 col-xs-4">
+                    <img src="assets/common/img/logo-z.png" class="img-responsive brand">
+                </div>
 
-                <a class="navbar-brand" href="#">Project name</a>
+                <div class="col-md-3 col-sm-4 col-sm-offset-5 col-md-offset-7 hidden-xs">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Saldo på brukskonto</h3>
+                        </div>
+                        <div class="panel-body text-center">
+                            <h1>1000,-</h1>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div id="navbar" class="navbar-collapse collapse">
-                <form class="navbar-form navbar-right">
-                    <div class="form-group">
-                        <input type="text" placeholder="Email" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <input type="password" placeholder="Password" class="form-control">
-                    </div>
-                    <button type="submit" class="btn btn-success">Sign in</button>
-                </form>
-            </div><!--/.navbar-collapse -->
         </div>
-    </nav>
+    </header>
 
-    <div class="jumbotron">
-        <div class="container">
-            <h1>Hello, world!</h1>
-            <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-            <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
-        </div>
-    </div>
+    <?php
+        require_once("assets/common/inc/navbar.php");
+    ?>
 
     <div class="container">
+        <div class="alert alert-danger alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4>Warning!</h4>
+            <p>Better check yourself, you're not looking too good.</p>
+        </div>
+
         <div class="row">
-        <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Saldo på brukskonto</h3>
+                    </div>
+                    <div class="panel-body text-center">
+                        <h1>1000,-</h1>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Saldo på brukskonto</h3>
+                    </div>
+                    <div class="panel-body text-center">
+                        <h1>1000,-</h1>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Saldo på brukskonto</h3>
+                    </div>
+                    <div class="panel-body text-center">
+                        <h1>1000,-</h1>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-        <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+
+        <div class="row">
+            <div class="col-md-8">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Mål</h3>
+                    </div>
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                        <p><canvas id="myChart" width="400" height="400"></canvas></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default bg-vipps text-white">
+                    <div class="panel-body">
+                        <p><img src="assets/common/img/vipps-logo.png" class="img-responsive" style="padding:0 50px;"></p>
+                        <form>
+                            <div class="form-group">
+                              <label for="tlf">Telefonnummer</label>
+                              <input type="number" class="form-control" id="tlf" placeholder="Eks.: 543 45 321" required="">
+                            </div>
+
+                            <div class="form-group">
+                              <label for="sum">Beløp</label>
+                              <input type="number" class="form-control" id="sum" placeholder="Eks.: 100" required="">
+                            </div>
+
+                            <button type="submit" name="button" class="btn btn-default">Vipps!</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
 
         <hr>
 
         <footer>
-            <p>&copy; 2015 Company, Inc.</p>
+            <p>&copy; PJ3100 Gruppe 15 - høsten 2016, vår 2017<br>
+            <?=$visitorIP?></p>
         </footer>
     </div><!-- /container -->
 
 
     <!-- Import JavaScript -->
     <script src="assets/lib/jquery-3.1.1/jquery-3.1.1.min.js"></script>
-    <script src="assets/lib/bootstrap-3.3.7.js/bootstrap.js"></script>
+    <script src="assets/lib/bootstrap-3.3.7/js/bootstrap.js"></script>
+    <script src="assets/lib/bootstrap-toggle/js/bootstrap-toggle.js"></script>
+    <script src="assets/lib/chartjs-2.3.0/Chart.js"></script>
+
+    <script>
+        var ctx = document.getElementById("myChart");
+        var myChart = new Chart(ctx, {
+            type: 'horizontalBar',
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+    </script>
 </body>
 </html>
