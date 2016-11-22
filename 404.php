a1fad4f002d6a533beeda5b5e943ec36ceb99199
sauenMalin <sauenmalin@gmail.com>
2016-11-22 13:17:08 +0100

    started on the error page

---
+++ 404.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Gruppe 15">
+
+    <title><?=$conf['sitename']?></title>
+
+    <!-- Import stylesheets -->
+    <link href="assets/common/css/stylesheet.css" rel="stylesheet">
+</head>
+<body>
+    <div class="site-wrapper">
+        <div class="site-wrapper-inner">
+          <div class="cover-container">
+            <div class="inner cover">
+              <h1 class="cover-heading">404</h1>
+              <p class="lead">
+                The page you were looking for doesn't exist or another error occured.
+              </p>
+            </div>
+          </div>
+        </div>
+    </div><!-- /container -->
+
+
+    <!-- Import JavaScript -->
+    <script src="assets/lib/jquery-3.1.1/jquery-3.1.1.min.js"></script>
+    <script src="assets/lib/bootstrap-3.3.7/js/bootstrap.js"></script>
+    <script src="assets/lib/bootstrap-toggle/js/bootstrap-toggle.js"></script>
+    <script src="assets/lib/chartjs-2.3.0/Chart.js"></script>
+
+</body>
+</html>
