fc4cf90d763593b93a9206e971e102d0b2954b26
Mathias Tollerud <mathias.tollerud@gmail.com>
2016-11-22 15:42:33 +0100

    Added Satan php class

---
+++ class/Satan.php
@@ -0,0 +1,32 @@
+<?php
+class Satan{
+
+    private static $instance;
+	private static $server = "http://10.32.13.28:8080/";
+
+    public static function init(){
+        if(self::$instance == NULL){
+            $className = __CLASS__;
+            self::$instance = new $className();
+        }
+        return self::$instance;
+    }
+
+    private function __construct(){
+
+    }
+
+	private function getJson($type){
+		$data = self::$server . $type;
+		$jsondata = file_get_contents($data);
+		$json = json_decode($jsondata, true);
+
+		return $json;
+	}
+
+	public function getUsers(){
+		$data = $this->getJson("customers");
+
+		return $data;
+	}
+}
