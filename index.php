0b62844a3584ea6183821ec0292bebac063655a1
Citona Marie Rygg <rygcit15@student.westerdals.no>
2016-11-22 13:10:25 +0100

    Moved header to a separate file

--- index.php
+++ index.php
@@ -18,28 +18,9 @@
     <link href="assets/common/css/stylesheet.css" rel="stylesheet">
 </head>
 <body>
-    <header>
-        <div class="container">
-            <div class="row valign">
-                <div class="col-md-2 col-sm-3 col-xs-4">
-                    <img src="assets/common/img/logo-z.png" class="img-responsive brand">
-                </div>
-
-                <div class="col-md-3 col-sm-4 col-sm-offset-5 col-md-offset-7 hidden-xs">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">Saldo på brukskonto</h3>
-                        </div>
-                        <div class="panel-body text-center">
-                            <h1>1000,-</h1>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
 
     <?php
+        require_once("assets/common/inc/header.php");
         require_once("assets/common/inc/navbar.php");
     ?>
 
