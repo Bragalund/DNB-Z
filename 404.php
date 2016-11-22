fa54c669acc3dbc08da00e5036f42c87bd82b2fb
sauenMalin <sauenmalin@gmail.com>
2016-11-22 15:39:16 +0100

    Done styling the 404

--- 404.php
+++ 404.php
@@ -1,26 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Gruppe 15">
-
-    <title><?=$conf['sitename']?></title>
-
-    <!-- Import stylesheets -->
-    <link href="assets/common/css/stylesheet.css" rel="stylesheet">
-</head>
+<?php
+require_once("assets/common/inc/head.php");
+?>
 <body id="error">
     <div class="site-wrapper">
         <div class="site-wrapper-inner">
           <div class="cover-container">
             <div class="inner cover">
               <h1 class="cover-heading">404</h1>
+              <h3>Page not found</h3>
               <p class="lead">
                 The page you were looking for doesn't exist or another error occured.
               </p>
+              <div class="btn-group" role="group">
+                <button type="button" class="btn btn-default"><a href="index.php">Tilbake til forsiden</a></button>
+              </div>
             </div>
           </div>
         </div>
