dbb31f6f592636f06c87cbc2e35d950f9f69d213
Citona Marie Rygg <rygcit15@student.westerdals.no>
2016-11-23 11:07:04 +0100

    Fixed some problems

--- accountOverview.php
+++ accountOverview.php
@@ -29,64 +29,68 @@ require_once("assets/common/inc/navbar.php");
 						</tr>
 					</thead>
 					<tbody>
-						<td>Citona Marie Rygg</td>
-						<td>1092.06.82408</td>
-						<td>1.000.000,-</td>
-						<td class="text-right text-xs-left">
-							<div class="btn-group">
-								<button type="button" class="btn btn-primary">Betale</button>
-								<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									<span class="caret"></span>
-									<span class="sr-only">Toggle Dropdown</span>
-								</button>
-								<ul class="dropdown-menu">
-									<li><a href="#">Betale</a></li>
-									<li><a href="#">Overføre</a></li>
-									<li role="separator" class="divider"></li>
-									<li><a href="#">Trenger vi denne?</a></li>
-								</ul>
-							</div>
-						</td>
-					</tbody>
-					<tbody>
-						<td>Mathias Tollerud</td>
-						<td>1662.02.83672</td>
-						<td>1000,-</td>
-						<td class="text-right text-xs-left">
-							<div class="btn-group">
-								<button type="button" class="btn btn-primary">Betale</button>
-								<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									<span class="caret"></span>
-									<span class="sr-only">Toggle Dropdown</span>
-								</button>
-								<ul class="dropdown-menu">
-									<li><a href="#">Betale</a></li>
-									<li><a href="#">Overføre</a></li>
-									<li role="separator" class="divider"></li>
-									<li><a href="#">Trenger vi denne?</a></li>
-								</ul>
-							</div>
-						</td>
-					</tbody>
-					<tbody>
-						<td>Malin Glosli Haugan</td>
-						<td>1422.05.92538</td>
-						<td>-100,-</td>
-						<td class="text-right text-xs-left">
-							<div class="btn-group">
-								<button type="button" class="btn btn-primary">Betale</button>
-								<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									<span class="caret"></span>
-									<span class="sr-only">Toggle Dropdown</span>
-								</button>
-								<ul class="dropdown-menu">
-									<li><a href="#">Betale</a></li>
-									<li><a href="#">Overføre</a></li>
-									<li role="separator" class="divider"></li>
-									<li><a href="#">Trenger vi denne?</a></li>
-								</ul>
-							</div>
-						</td>
+						<tr>
+							<td>Citona Marie Rygg</td>
+							<td>1092.06.82408</td>
+							<td>1.000.000,-</td>
+							<td class="text-right text-xs-left">
+								<div class="btn-group">
+									<button type="button" class="btn btn-primary">Betale</button>
+									<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										<span class="caret"></span>
+										<span class="sr-only">Toggle Dropdown</span>
+									</button>
+									<ul class="dropdown-menu">
+										<li><a href="#">Betale</a></li>
+										<li><a href="#">Overføre</a></li>
+										<li role="separator" class="divider"></li>
+										<li><a href="#">Trenger vi denne?</a></li>
+									</ul>
+								</div>
+							</td>
+						</tr>
+						<tr>
+							<td>Mathias Tollerud</td>
+							<td>1662.02.83672</td>
+							<td>1000,-</td>
+							<td class="text-right text-xs-left">
+								<div class="btn-group">
+									<button type="button" class="btn btn-primary">Betale</button>
+									<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										<span class="caret"></span>
+										<span class="sr-only">Toggle Dropdown</span>
+									</button>
+									<ul class="dropdown-menu">
+										<li><a href="#">Betale</a></li>
+										<li><a href="#">Overføre</a></li>
+										<li role="separator" class="divider"></li>
+										<li><a href="#">Trenger vi denne?</a></li>
+									</ul>
+								</div>
+							</td>
+						</tr>
+						<tr>
+
+
+							<td>Malin Glosli Haugan</td>
+							<td>1422.05.92538</td>
+							<td>-100,-</td>
+							<td class="text-right text-xs-left">
+								<div class="btn-group">
+									<button type="button" class="btn btn-primary">Betale</button>
+									<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										<span class="caret"></span>
+										<span class="sr-only">Toggle Dropdown</span>
+									</button>
+									<ul class="dropdown-menu">
+										<li><a href="#">Betale</a></li>
+										<li><a href="#">Overføre</a></li>
+										<li role="separator" class="divider"></li>
+										<li><a href="#">Trenger vi denne?</a></li>
+									</ul>
+								</div>
+							</td>
+						</tr>
 					</tbody>
 				</table>
 			</div><!-- /panel -->
