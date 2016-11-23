ad8b0a6d3c5e0c70f2c10918750590d00b81f79b
Citona Marie Rygg <rygcit15@student.westerdals.no>
2016-11-23 09:59:55 +0100

    Fixed some errors

--- assets/common/inc/scripts.php
+++ assets/common/inc/scripts.php
@@ -3,4 +3,4 @@
 <script src="assets/lib/bootstrap-3.3.7/js/bootstrap.js"></script>
 <script src="assets/lib/bootstrap-toggle/js/bootstrap-toggle.js"></script>
 <script src="assets/lib/chartjs-2.3.0/Chart.js"></script>
-<script src="assets/lib/stackable-1.0.1/stackable.js"></script>
+<script src="assets/lib/stackable-1.0.1/stacktable.js"></script>
